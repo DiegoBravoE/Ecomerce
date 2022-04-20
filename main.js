d960740f31240e734b62b1c1f873911bd7250a5f
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-20 17:50:12 -0300

    js json

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ window.addEventListener("scroll",function(){
     let nav = document.querySelector("nav")
     nav.classList.toggle("sticky",window.scrollY > 0)
 })
+const productos={ 
+    "stock":
+[      
+   {
+    "modelo":"Hoodies",
+    "unidades":3
+    },
+    {
+    "modelo":"Shirts",
+    "unidades":4
+      },
+     {
+    "modelo":"Sweatshirts",
+    "unidades":5
+    }
+
+]
+}   
+const boton=document.querySelector(".agregar")
+boton.addEventListener("click",()=>{
+    let carrito= localStorage.getItem("buzo")
+alert(carrito)
+});
