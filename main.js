9bf92b24c08414c18ece3ce43989df7cf313b42a
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-22 15:15:10 -0300

    carrito

--- main.js
+++ main.js
@@ -3,48 +3,73 @@ window.addEventListener("scroll",function(){
     let nav = document.querySelector("nav")
     nav.classList.toggle("sticky",window.scrollY > 0)
 })
-const productos={ 
-    "stock":
-[      
-   {
-   "imagen": "/img/buzo-wilson.jpg", 
-    "modelo":"Hoodies",
-    "unidades":3,
-    "precio": "$10.00"
-    },
-    {
-    "imagen": "/img/crazy-black-21.png",
-    "modelo":"Shirts",
-    "unidades":4,
-    "precio": "$15.00"
-      },
-     {
-    "imagen": "/img/buzo-rosa.png",    
-    "modelo":"Sweatshirts",
-    "unidades":5,
-    "precio": "$20.00"
-    }
 
-]
-}   
+
+    
+const productos=[
+
+                { 
+                "imagen": "/img/buzo-wilson.jpg", 
+                "modelo":"Hoodies",
+                "unidades":3,
+                "precio": "$10.00"
+                },
+                {
+                "imagen": "/img/crazy-black-21.png",
+                "modelo":"Shirts",
+                "unidades":4,
+                "precio": "$15.00"
+                },
+                {
+                "imagen": "/img/buzo-rosa.png",    
+                "modelo":"Sweatshirts",
+                "unidades":5,
+                "precio": "$20.00"
+                }
+               ]
+
+
+
+   
 const carrito=[]
+const agregarProducto = ()=>{
+    carrito.push(productos)
+    localStorage.setItem("productos",JSON.stringify(productos))    
+    
+  
+}
+agregarProducto()
 
 
-const lista= document.getElementById("seleccion1")
 
 
 const producto1=document.querySelector(".producto1")
-producto1.addEventListener("click",()=> {
+     producto1.addEventListener("click",()=> {
 
-    agregarProducto()
-   console.log(carrito)
-      
+     const carritoParseado=JSON.parse(localStorage.getItem("productos"))
+     producto1.innerHTML +=
+ 
+     `
+     <section class="modal" id="modal">
+
+            <div class="modal-container">
+                <div id="seleccion1">
+                <div class="agregar">
+                <h6>${ carritoParseado[0].imagen}</h6>
+                <h6>${ carritoParseado[0].modelo}</h6>
+                <h6>${ carritoParseado[0].precio}</h6>
+            </div> 
+                 </div>
+                 <a href="#producto" class="close">x</a>
+                 <button class="delete" >eliminar</button>
+             </div>
+        </section>
+            
+      `
+     console.log(carritoParseado[0])
+    
 })
-const agregarProducto = ()=>{
-    carrito.push(productos.stock[0])
-    localStorage.setItem("carrito",JSON.stringify(carrito))    
-   
-  
-}
-const carritoParseado=JSON.parse(localStorage.getItem("carrito"))
+
+
+
 
