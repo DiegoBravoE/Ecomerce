b43776196c90c4847f6cebc24041fb8ecf66865b
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-22 00:27:24 -0300

    modal mobile

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ const productos={
     "stock":
 [      
    {
+   "imagen": "/img/buzo-wilson.jpg", 
     "modelo":"Hoodies",
     "unidades":3,
     "precio": "$10.00"
     },
     {
+    "imagen": "/img/crazy-black-21.png",
     "modelo":"Shirts",
     "unidades":4,
     "precio": "$15.00"
       },
      {
+    "imagen": "/img/buzo-rosa.png",    
     "modelo":"Sweatshirts",
     "unidades":5,
     "precio": "$20.00"
